c4c2691d6a23d0306011031f4d6dc9b148f9c2f6
Kaizilege Karoma <kkaroma@gmail.com>
2025-02-12 10:32:25 +0300

    Display recent added books on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+      # @books = Book.order("id DESC")
+
+      @books = Book.all.order("id DESC").limit(4)
+
+    # @books = @book.order("id DESC")
   end
 end
