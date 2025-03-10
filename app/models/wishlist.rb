67dd476292df70bedb32d6591ff8d69e0c5468bc
Kaizilege Karoma <kkaroma@gmail.com>
2025-03-10 12:29:00 +0300

    Added Add to withlist button to books user do not own

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -2,5 +2,9 @@ class Wishlist < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-  validates :book_id, uniqueness: { scope: :user_id, message: "is already in your wishlist" }
+  # validates :book_id, uniqueness: { scope: :user_id, message: "is already in your wishlist" }
+  validates :user_id, uniqueness: { scope: :book_id, message: "has already wishlisted this book" }
 end
+
+belongs_to :user
+  belongs_to :book
