8b70a2f0c750f473aa11dc5f91fae4085fc6e10d
Kaizilege Karoma <kkaroma@gmail.com>
2025-03-11 15:49:43 +0300

    Clear error on wishlist model

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -5,6 +5,3 @@ class Wishlist < ApplicationRecord
   # validates :book_id, uniqueness: { scope: :user_id, message: "is already in your wishlist" }
   validates :user_id, uniqueness: { scope: :book_id, message: "has already wishlisted this book" }
 end
-
-belongs_to :user
-  belongs_to :book
